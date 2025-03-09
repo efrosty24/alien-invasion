a169d45a10d3daf0cf5c18d2caf53f8d20048bd1
Evan Fraustro <xzander0110@gmail.com>
2025-03-08 21:17:34 -0600

    New ship speed updater

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,6 +24,7 @@ class AlienInvasion:
 
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -32,6 +33,19 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # move the ship to the right
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    # move the ship to the left
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False 
 
     def _update_screen(self):
         '''update images on the screen and flip to another screen'''
--- ship.py
+++ ship.py
@@ -1,10 +1,11 @@
 import pygame
-
+from settings import Settings 
 class Ship:
     '''A class to manage the ship'''
     def __init__(self, ai_game):
 
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #load the ship image and get its rect
@@ -13,6 +14,24 @@ class Ship:
         self.rect = self.image.get_rect()
 
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a float for the ships's exact horizontal position
+        self.x = float(self.rect.x)
+
+        # Movement flag; start with a ship not moving
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        ''' Update the ship's position based on the movement flag'''
+        # Update the ship's x value, not the rectangle
+        if self.moving_right:
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         '''Draw the ship at its current location'''
